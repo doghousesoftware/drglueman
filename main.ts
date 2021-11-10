0d4c1a559d30eb945473d0f20a335c41a44ba2c2
bryan weinstein <bryanweinstein@outlook.com>
2021-11-10 12:58:34 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+function turnServo (speed: number) {
+    servos.P2.run(speed)
+}
+// convert input to a percentage for the servo
+// and also display input as a percentage of 43
+function velocityConversion (input2: number) {
+    basic.showNumber(Math.round(input2 / maxServo * 43))
+    turnServo(input2 / maxServo * 100)
+}
+let maxServo = 0
+maxServo = 1023
+basic.forever(function () {
+    velocityConversion(pins.analogReadPin(AnalogPin.P0))
+})
